5660d8aa789c59ae22deff598395e2f0e14a6d01
Your Name <you@example.com>
2021-02-26 15:49:52 +0500

    add hw 1

---
+++ HW_1_1_namewrite/main.cpp
@@ -0,0 +1,17 @@
+/* Задание 1 - написать первую букву своего имени */
+
+#include <iostream>
+
+int main()
+{
+    std::cout << "***********" << std::endl
+              << "***********" << std::endl
+              << "**       **" << std::endl
+              << "**       **" << std::endl
+              << "**       **" << std::endl
+              << "**       **" << std::endl
+              << "**       **" << std::endl;
+
+
+    return 0;
+}
