57f1749d1141340d7abc7cbe5a969646e4be6517
Polina <79652001+Polina120A@users.noreply.github.com>
2021-05-28 22:38:30 +0500

    Add files via upload

---
+++ sum_massiv1/main.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+
+void inputRandArray(int a[], int n){
+    for(int i=0; i<n; i++)
+        a[i] = rand() % 56;
+}
+void outputArray(int a[], int n){
+    for(int i=0; i<n; i++)
+        std::cout << a[i] << ", ";
+    std::cout << std::endl;
+}
+int sumMore(int a[], int n, int b){
+    int sum = 0;
+    for(int i=0; i<n; i++)
+        if(a[i]>b)
+        sum += a[i];
+    return sum;
+}
+
+int main()
+{
+    const int n = 25;
+     int arr[n];
+     srand(2547);
+     inputRandArray(arr, n);
+     outputArray(arr, n);
+
+     std::cout << "SumIf = " << sumMore(arr, n, 3) << std::endl;
+
+    return 0;
+}
